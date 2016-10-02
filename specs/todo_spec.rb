3a2a7a49d506200b85304e281ba26554150fe1f5
Alex Bazlinton <abazlinton@gmail.com>
2016-10-02 18:15:53 +0100

    Basic data structure

---
+++ specs/todo_spec.rb
@@ -0,0 +1,52 @@
+require 'pry-byebug'
+require 'minitest/autorun'
+require 'minitest/rg'
+
+class TestToDo < MiniTest::Test
+
+def setup
+
+	@lists = [
+		{
+			name: "Personal",
+			todos: [
+				text: "Washing",
+				done: false
+
+			]
+		},
+		{
+			name: "CodeClan",
+			todos: [
+				text: "Homework",
+				done: false
+
+			]
+
+
+
+		}
+
+	]
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
