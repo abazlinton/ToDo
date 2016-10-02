4c637d78b093d86866ab3408410ff6dcdfc2e622
Alex Bazlinton <abazlinton@gmail.com>
2016-10-02 18:45:31 +0100

    First 4 tests created

--- specs/todo_spec.rb
+++ specs/todo_spec.rb
@@ -10,25 +10,47 @@ def setup
 		{
 			name: "Personal",
 			todos: [
-				text: "Washing",
-				done: false
+					{text: "Washing", done: false},
+					{text: "Make Dinner", done: false}
+
 			]
 		},
 		{
 			name: "CodeClan",
 			todos: [
-				text: "Homework",
-				done: false
+
+					{text: "Homework", done: false},
+					{text: "Update README", done: true}
 
 			]
 		}
 
 	]
+end
 
 
+	def test_outstanding_count_list
+		count = outstanding(@lists, "Personal")
+		assert_equal(2, count)
+	end
 
+	def test_outstanding_count_global
+		count = outstanding(@lists)
+		assert_equal(3, count)
+	end
 
+	def test_outstanding_todos_list
+		todos = todos_outstanding(@lists, "CodeClan")
+		assert_equal( "Array", todos.class) 
+		assert_equal( 1, todos.count)
+	end
 
+	def test_outstanding_todos_global
+		todos = outstanding(@lists)
+		assert_equal( "Array", todos.class) 
+		assert_equal( 3, todos.class)
+
+	end
 
 end
 
@@ -40,9 +62,3 @@ end
 
 
 
-
-
-
-
-
-end
