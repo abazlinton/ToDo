d0c2db445472b91d8f39c50b1f1d8b91d0665298
Alex Bazlinton <abazlinton@gmail.com>
2016-10-02 18:49:16 +0100

    fix a test

--- specs/todo_spec.rb
+++ specs/todo_spec.rb
@@ -31,25 +31,28 @@ end
 
   def test_outstanding_count_list
     count = outstanding(@lists, "Personal")
+
     assert_equal(2, count)
   end
 
   def test_outstanding_count_global
     count = outstanding(@lists)
+
     assert_equal(3, count)
   end
 
   def test_outstanding_todos_list
     todos = todos_outstanding(@lists, "CodeClan")
+
     assert_equal( "Array", todos.class) 
     assert_equal( 1, todos.count)
   end
 
   def test_outstanding_todos_global
     todos = outstanding(@lists)
-    assert_equal( "Array", todos.class) 
-    assert_equal( 3, todos.class)
 
+    assert_equal( "Array", todos.class) 
+    assert_equal( 3, todos.count)
   end
 
 end
