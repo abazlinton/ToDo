224769cac6e7f9268c5d6072917d8354431feb26
Alex Bazlinton <abazlinton@gmail.com>
2016-10-02 19:56:43 +0100

    Indentation now 3 spaces

--- todo.rb
+++ todo.rb
@@ -1,35 +1,35 @@
 def outstanding_count_list(lists, list_name)
 
-	for list in lists
-		if list[:name] == list_name
-			return list[:todos].length
-		end
-	end
-	return nil
+   for list in lists
+      if list[:name] == list_name
+         return list[:todos].length
+      end
+   end
+   return nil
 end
 
 def outstanding_count_global(lists)
-	return nil if lists == []
-	global_count = 0
-	for list in lists
-		global_count += outstanding_count_list(lists, list[:name])
-	end
-	return global_count
+   return nil if lists == []
+   global_count = 0
+   for list in lists
+      global_count += outstanding_count_list(lists, list[:name])
+   end
+   return global_count
 end
 
 def outstanding_todos_list(lists, list_name)
-	todos = []
-	for list in lists
-		if list[:name] == list_name
-			return nil if list[:todos] == []
-			for todo in list[:todos]
-				if todo[:done] == false
-					todos << todo
-				end
-			end
-		end
-	end
-	return todos
+   todos = []
+   for list in lists
+      if list[:name] == list_name
+         return nil if list[:todos] == []
+         for todo in list[:todos]
+            if todo[:done] == false
+               todos << todo
+            end
+         end
+      end
+   end
+   return todos
 
 end
 
