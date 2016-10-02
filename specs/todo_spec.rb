861b9b43ef60591913115726899ba1c50cc566c6
Alex Bazlinton <abazlinton@gmail.com>
2016-10-02 18:48:14 +0100

    attempt to fix indenting on GitHub

--- specs/todo_spec.rb
+++ specs/todo_spec.rb
@@ -6,51 +6,51 @@ class TestToDo < MiniTest::Test
 
 def setup
 
-	@lists = [
-		{
-			name: "Personal",
-			todos: [
-					{text: "Washing", done: false},
-					{text: "Make Dinner", done: false}
+  @lists = [
+    {
+      name: "Personal",
+      todos: [
+          {text: "Washing", done: false},
+          {text: "Make Dinner", done: false}
 
-			]
-		},
-		{
-			name: "CodeClan",
-			todos: [
+      ]
+    },
+    {
+      name: "CodeClan",
+      todos: [
 
-					{text: "Homework", done: false},
-					{text: "Update README", done: true}
+          {text: "Homework", done: false},
+          {text: "Update README", done: true}
 
-			]
-		}
+      ]
+    }
 
-	]
+  ]
 end
 
 
-	def test_outstanding_count_list
-		count = outstanding(@lists, "Personal")
-		assert_equal(2, count)
-	end
+  def test_outstanding_count_list
+    count = outstanding(@lists, "Personal")
+    assert_equal(2, count)
+  end
 
-	def test_outstanding_count_global
-		count = outstanding(@lists)
-		assert_equal(3, count)
-	end
+  def test_outstanding_count_global
+    count = outstanding(@lists)
+    assert_equal(3, count)
+  end
 
-	def test_outstanding_todos_list
-		todos = todos_outstanding(@lists, "CodeClan")
-		assert_equal( "Array", todos.class) 
-		assert_equal( 1, todos.count)
-	end
+  def test_outstanding_todos_list
+    todos = todos_outstanding(@lists, "CodeClan")
+    assert_equal( "Array", todos.class) 
+    assert_equal( 1, todos.count)
+  end
 
-	def test_outstanding_todos_global
-		todos = outstanding(@lists)
-		assert_equal( "Array", todos.class) 
-		assert_equal( 3, todos.class)
+  def test_outstanding_todos_global
+    todos = outstanding(@lists)
+    assert_equal( "Array", todos.class) 
+    assert_equal( 3, todos.class)
 
-	end
+  end
 
 end
 
